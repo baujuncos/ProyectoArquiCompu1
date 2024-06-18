f878338e305a40269f83159ca0891c2a2b4c3dfb
baujuncos <127853844+baujuncos@users.noreply.github.com>
2024-06-18 11:37:52 -0300

    Add files via upload

---
+++ assembly_functions.h
@@ -0,0 +1,5 @@
+#ifndef PROYECTO_ASSEMBLY_FUNCTIONS_H
+#define PROYECTO_ASSEMBLY_FUNCTIONS_H
+extern void parpadeoAlternado(void);
+extern void olaOceanica(void);
+#endif //PROYECTO_ASSEMBLY_FUNCTIONS_H
